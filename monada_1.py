21c4b404ed101648f3286a5dc7f14893ce28a94a
Alexandr Volkov <volkov.alexandr.pavl@gmail.com>
2022-01-20 19:36:41 +0300

    monada

---
+++ monada_1.py
@@ -0,0 +1,60 @@
+class User:
+    def __init__(self, name, friends=None):
+        self.name = name
+        if friends is None:
+            self.friends = []
+
+    def get_friend_by_index(self, index):
+        try:
+            return self.friends[index]
+        except IndexError:
+            return None
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+user = User('main')
+friend = User('1')
+friend.friends = [User('1.1'), User('1.2'), User('1.3')]
+user.friends = [friend]
+
+
+class Maybe:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def unit(cls, value):
+        return cls(value)
+
+    def bind(self, f):
+        if self.value is None:
+            return self
+
+        result = f(self.value)
+        if isinstance(result, Maybe):
+            return result
+        return Maybe(result)
+
+    def __getattr__(self, name):
+        # TODO: if hasattr(self.value, name) and getattr(self.value, name) then exception
+        field = hasattr(self.value, name) and getattr(self.value, name) or None
+        if not callable(field) or not field:
+            return self.bind(lambda _: field)
+        return lambda *args, **kwargs: self.bind(lambda _: field(*args, **kwargs))
+
+
+def first_value(values):
+    if len(values) > 0:
+        return values[0]
+    return None
+
+
+friends = (
+    Maybe.unit(None).
+    get_friend_by_index(1).
+    friends.
+    value
+)
+print(friends)
